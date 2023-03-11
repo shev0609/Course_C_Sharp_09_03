e69aad2854c92a2c5ea259f20b426abd5e2293e4
shev0609 <m.lolita653@gmail.com>
2023-03-11 14:16:47 +0500

    Task 1_0

---
+++ Lession/1_2/Program.cs
@@ -0,0 +1,13 @@
+﻿// 2. Напишите программу, которая будет выдавать название дня
+//    недели по заданному номеру.
+
+string s_a = Console.ReadLine()!;
+string s_b = Console.ReadLine()!;
+
+int a = int.Parse(s_a);
+int b = int.Parse(s_b);
+
+if (a == b * b)  Console.WriteLine("Yes");
+else  Console.WriteLine("No");
+
+ // Console.WriteLine("Hello, World!");
